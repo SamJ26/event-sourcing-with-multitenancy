95f693fc096bb0a08674cab61c2116b02f5df218
Samuel Janek <samueljanek17@gmail.com>
2024-02-21 20:26:01 +0100

    Add comment to TenantContextMiddleware

--- TenantContextProvider.cs
+++ TenantContextProvider.cs
@@ -2,6 +2,7 @@ namespace EventSourcing;
 
 public sealed class TenantContextProvider
 {
+    // In real application, this value would be set in the middleware
     public TenantContext Current => new TenantContext("Google", "tenant_google");
 }
 
